72f719cc93b029974365ba66fc5ee3af4374ab9a
Chris Thoburn <runspired@gmail.com>
2015-02-16 13:11:51 -0600

    Release v1.3.0

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(/* environment, appConfig */) {
   return {
-    version : "1.2.2",
+    version : "1.3.0",
     mobileTouch : {
       useGesturesHash : false,
       alwaysTapOnPress : false,
