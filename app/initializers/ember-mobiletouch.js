c26cd002778672ad2dde9b9c304377c4c900d4ab
Chris Thoburn <runspired@gmail.com>
2015-02-17 09:30:39 -0600

    htmlbars compatibility

--- app/initializers/ember-mobiletouch.js
+++ app/initializers/ember-mobiletouch.js
@@ -4,13 +4,16 @@ import HammerEvents from 'ember-mobiletouch/mixins/hammer-events';
 import GesturesMixin from 'ember-mobiletouch/mixins/gestures';
 import LinkViewMods from 'ember-mobiletouch/mixins/link-view-mods';
 
+var USE_HTMLBARS = !!Ember.HTMLBars;
+
 export default {
 
   name: 'mobiletouch',
 
   initialize: function(container) {
 
-    var dispatcher = Ember.EventDispatcher.extend({ _mobileTouchConfig : config.mobileTouch || {} }, HammerEvents);
+    var dispatcher = Ember.EventDispatcher.extend({ _mobileTouchConfig : config.mobileTouch || {} }, HammerEvents),
+      oldActionHelper;
 
     //components extend Ember.View, so this should be all that's needed
     Ember.View.reopen(GesturesMixin, {
@@ -19,13 +22,25 @@ export default {
 
     Ember.LinkView.reopen({ __defaultTapOnPress : config.mobileTouch.defaultTapOnPress || false }, LinkViewMods);
 
-    var oldActionHelper = Ember.Handlebars.helpers.action;
-    Ember.Handlebars.helpers.action = function (/*actionName*/) {
-      var options = arguments[arguments.length - 1];
-      var hash = options.hash;
-      hash.on = hash.on || 'tap';
-      return oldActionHelper.apply(this, arguments);
-    };
+    if (USE_HTMLBARS) {
+      //do nothing for a moment
+      oldActionHelper = Ember.Handlebars.helpers.action.helperFunc;
+      Ember.Handlebars.helpers.action.helperFunc = function (/*actionName*/) {
+        var options = arguments[arguments.length - 1];
+        var hash = options.hash;
+        hash.on = hash.on || 'tap';
+        return oldActionHelper.apply(this, arguments);
+      };
+
+    } else {
+      oldActionHelper = Ember.Handlebars.helpers.action;
+      Ember.Handlebars.helpers.action = function (/*actionName*/) {
+        var options = arguments[arguments.length - 1];
+        var hash = options.hash;
+        hash.on = hash.on || 'tap';
+        return oldActionHelper.apply(this, arguments);
+      };
+    }
 
     container.register('event_dispatcher:main', dispatcher);
 
