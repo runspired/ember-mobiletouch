8e09485357a2493fd2fac183461c84aed7841a78
Kevin Bullaughey <kbullaughey@gmail.com>
2015-02-25 15:52:53 -0500

    relying on direct import statement rather than initializer for modification of ember classes; this plays better with ember-test-helpers and ensures that the setup code is executed consistently and not executed multiple times.

--- app/overrides/ember-mobiletouch.js
+++ app/overrides/ember-mobiletouch.js
@@ -1,40 +1,34 @@
 import Ember from 'ember'; //event dispatcher
 import config from '../config/environment'; //ENV.hammer
-import HammerEvents from 'ember-mobiletouch/mixins/hammer-events';
 import GesturesMixin from 'ember-mobiletouch/mixins/gestures';
 import LinkViewMods from 'ember-mobiletouch/mixins/link-view-mods';
+import HammerEvents from 'ember-mobiletouch/mixins/hammer-events';
 
 var USE_HTMLBARS = !!Ember.HTMLBars;
 
-export default {
+var overrides = function() {
+    var oldActionHelper;
+    var defaultTapOnPress = config.mobileTouch.defaultTapOnPress || true;
+    var mobileTouchConfig = { _mobileTouchConfig : config.mobileTouch || {} };
 
-  name: 'mobiletouch',
-
-  initialize: function(container) {
-
-    var dispatcher = Ember.EventDispatcher.extend({ _mobileTouchConfig : config.mobileTouch || {} }, HammerEvents),
-      defaultTapOnPress = config.mobileTouch.defaultTapOnPress || true,
-      oldActionHelper;
+    // modify the dispatcher
+    Ember.EventDispatcher.reopen(mobileTouchConfig);
+    Ember.EventDispatcher.reopen(HammerEvents);
 
     //components extend Ember.View, so this should be all that's needed
     Ember.View.reopen(GesturesMixin, {
       __useGesturesHash : config.mobileTouch ? config.mobileTouch.useGesturesHash : false
     });
-
     Ember.LinkView.reopen({ __defaultTapOnPress : defaultTapOnPress }, LinkViewMods);
 
     if (USE_HTMLBARS) {
-
       //cache the old handler
       oldActionHelper = Ember.Handlebars.helpers.action.helperFunction;
 
       Ember.Handlebars.helpers.action.helperFunction = function (params, hash, options, env) {
-
         hash.on = hash.on || 'tap';
         oldActionHelper.apply(this, arguments);
-
       };
-
     } else {
       oldActionHelper = Ember.Handlebars.helpers.action;
       Ember.Handlebars.helpers.action = function (/*actionName*/) {
@@ -44,8 +38,7 @@ export default {
         return oldActionHelper.apply(this, arguments);
       };
     }
-
-    container.register('event_dispatcher:main', dispatcher);
-
-  }
 };
+
+// overrides will be applied immediately and only once when the module is imported
+export default overrides();
