8c740d342319d398a4eb4eb4dabb0c6dc44d2b23
Chris Thoburn <runspired@gmail.com>
2015-03-03 15:59:35 -0600

    better api for custom recognizers

--- addon/recognizers.js
+++ addon/recognizers.js
@@ -1,23 +1,49 @@
 import capitalizeWord from "./utils/capitalize-word";
+import Ember from "Ember";
 
 export default function (EventManager, instance) {
 
+  var self = this;
   this.Recognizers = {};
   this.Manager = instance;
-  this.recognize = function (type, settings) {
+  this.recognize = function (recognizer) {
+
+    Ember.assert('Custom Recognizers must provide a name.  Names should be camelCased, but this is not enforced.', recognizer.name);
+    Ember.assert('Custom Recognizers must provide the name of the gesture class to use', recognizer.gesture);
+
+    var key = capitalizeWord(recognizer.name),
+      eventName = recognizer.name.toLowerCase();
 
     //add the recognizer
-    var event = settings.event;
-    settings.event = settings.event.toLowerCase();
-    var recognizer = new Hammer[capitalizeWord(type)](settings);
-    this.Recognizers[settings.event] = recognizer;
-    this.Manager.add(recognizer);
+    recognizer.tune = recognizer.tune || {};
+    recognizer.tune.event = eventName;
+    var Recognizer = new Hammer[capitalizeWord(recognizer.gesture)](recognizer.tune);
+
+    //recognizeWith
+    if (recognizer.with) {
+      Recognizer.recognizeWith(recognizer.with.map(function(name) {
+        return self.Recognizers[capitalizeWord(name)];
+      }));
+    }
+
+    //recognizeFailure
+    if (recognizer.without) {
+      Recognizer.requireFailure(recognizer.with.map(function(name) {
+        return self.Recognizers[capitalizeWord(name)];
+      }));
+    }
+
+    //add to the manager and register
+    this.Recognizers[key] = Recognizer;
+    this.Manager.add(Recognizer);
 
     //add the ember event
-    this._setupEvent(event);
+    this._setupEvent(recognizer.name);
 
   };
 
+
+  //add the event to Ember's eventing
   this._setupEvent = function (name) {
     var events = EventManager.get('events');
     events[name.toLowerCase()] = name;
--- app/recognizers.js
+++ app/recognizers.js
@@ -8,15 +8,28 @@ export default function () {
   //this.Recognizers is a hash of available recognizers
   //   e.g. this.Recognizers.Pan
 
-  //you can add a new recognizer, for instance doubletap, like so
-  /*
 
-  ```
-  this.recognize('tap', { event : 'doubleTap', taps : 2 });
-  this.Recognizers.doubleTap.recognizeWith(this.Recognizers.Tap);
-  ```
-   */
-  //the jQuery event would be `doubletap` and the Ember event would be `doubleTap`
+
+  //you can add a new recognizer, for instance doubleTap, like below
+  //the DOM event will be all lowercase (doubletap)
+  //the Ember event will be camelCase (doubleTap)
+  //the key in this.Recognizers will be SnakeCase (DoubleTap)
+  /*
+  this.recognize({
+
+    name : 'doubleTap', //always camelCase this
+
+    gesture : 'tap', //the base Hammer recognizer to use
+
+    tune : { //the settings to pass to the recognizer, event will be added automatically
+      taps : 2
+    },
+
+    with : ['tap'], //an array of recognizers to recognize with.
+
+    without : [], //an array of recognizers that must first fail
+  });
+  */
 
 
 }
