8e5b564de83ffd988f1990df6583b06612a8c3cd
Chris Thoburn <runspired@gmail.com>
2015-03-12 20:04:20 -0500

    another attempt at re-releasing 1.4.3 without npm shasum issues

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(/* environment, appConfig */) {
   return {
-    version : "1.4.4",
+    version : "1.4.5",
     mobileTouch : {
       useGesturesHash : false,
       alwaysTapOnPress : false,
