8c400fdd6f27de099fe42e12ce0b4ca78c566a44
Chris Thoburn <runspired@gmail.com>
2015-03-19 11:31:46 -0500

    updated tests, removed ghost click buster, better click busting

--- addon/overrides/event-dispatcher.js
+++ addon/overrides/event-dispatcher.js
@@ -1,5 +1,4 @@
 import Ember from "ember";
-import PreventGhostClicks from "../utils/prevent-ghost-clicks";
 import capitalizeWord from "../utils/capitalize-word";
 //import isCustomProtocol from "../utils/is-custom-protocol";
 import isGesture from "../utils/is-gesture";
@@ -70,17 +69,8 @@ export default Ember.EventDispatcher.reopen({
      We have to click bust clicks that trigger undesirable behaviors,
      but still allow clicks that do.
      */
+
     $root.on('click.ember-mobiletouch', '[data-ember-action]', function (e) {
-
-      //lock it down
-      //this unfortunately prevents submit behavior
-      e.preventDefault();
-      e.stopPropagation();
-      e.stopImmediatePropagation();
-
-    });
-
-    $root.on('click.ember-mobiletouch', function (e) {
 
       var $currentTarget = Ember.$(e.currentTarget);
 
@@ -162,16 +152,6 @@ export default Ember.EventDispatcher.reopen({
       });
 
     }
-
-    /*
-      Mobile devices trigger a click after a delay
-
-      Mobile Browsers try to pretend that they are normal browsers, so they fire a "click" event
-      a short delay after a "touchEnd". This is great for sites that haven't optimized for mobile,
-      but for sites that do use touchEnd to determine taps/clicks, the extra click event needs to
-      be captured and discarded, otherwise both the touchEnd and the click event will trigger a tap.
-     */
-    PreventGhostClicks.add(element);
 
   },
 
@@ -376,7 +356,6 @@ export default Ember.EventDispatcher.reopen({
 
     var hammer = this.get('_hammerInstance');
     var $element = Ember.$(this.get('rootElement'));
-    var element = $element.get(0);
 
     // Clean up edge case handlers
     $element.off('tap press click');
@@ -384,9 +363,6 @@ export default Ember.EventDispatcher.reopen({
     //teardown Hammer
     if (hammer) { hammer.destroy(); }
     this.set('_hammerInstance', null);
-
-    //teardown clickbuster
-    PreventGhostClicks.remove(element);
 
     //run normal destroy
     this._super();
