7510700d1cea60588271a2078b95bdf8d7b47828
Kevin Bullaughey <kbullaughey@gmail.com>
2015-06-16 11:01:34 -0400

    enhances to test case

--- tests/dummy/app/pods/forms/complete-it/component.js
+++ tests/dummy/app/pods/forms/complete-it/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'button',
-  classNames: ['btn'],
+  classNames: ['btn', 'btn-default'],
   type: 'submit',
   attributeBindings: ['type'],
 });
