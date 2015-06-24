14e2dce42502f68b40b58815248edb88ea17ad0b
Robert Jackson <robert.w.jackson@me.com>
2015-06-24 10:15:41 -0400

    Reopen Component and View for 1.13+ compat.
     Please enter the commit message for your changes. Lines starting

--- app/initializers/ember-mobiletouch.js
+++ app/initializers/ember-mobiletouch.js
@@ -3,6 +3,8 @@ import defaultConfig from 'ember-mobiletouch/default-config';
 
 //activate overrides
 import ModifiedView from 'ember-mobiletouch/overrides/view';
+import ModifiedComponent from 'ember-mobiletouch/overrides/component';
+import ModifiedCheckbox from 'ember-mobiletouch/overrides/checkbox';
 import ModifiedLinkView from 'ember-mobiletouch/overrides/link-view';
 import ModifiedEventDispatcher from '../overrides/ember-mobiletouch';
 import ModifiedActionHelper from 'ember-mobiletouch/overrides/action-helper';
@@ -18,6 +20,8 @@ export default {
 
     //add config settings to overrides
     ModifiedView.reopen({ __useGesturesHash : mergedConfig.useGesturesHash });
+    ModifiedComponent.reopen({ __useGesturesHash : mergedConfig.useGesturesHash });
+    ModifiedCheckbox.reopen({ __useGesturesHash : mergedConfig.useGesturesHash });
     ModifiedLinkView.reopen({ __defaultTapOnPress : mergedConfig.defaultTapOnPress });
 
   }
