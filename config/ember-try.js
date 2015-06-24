332ea2a359bf2c69cb6db09d7f0f698b890aa488
Robert Jackson <robert.w.jackson@me.com>
2015-06-23 22:42:06 -0400

    Add ember-try for testing multiple Ember releases.
    Adds testing for:
    
    * 1.10.0
    * 1.11.3
    * 1.12.1

---
+++ config/ember-try.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scenarios: [
+    {
+      name: "1.10.0",
+      dependencies: {
+        "ember": "1.10.0"
+      }
+    },
+    {
+      name: "1.11.3",
+      dependencies: {
+        "ember": "1.11.3"
+      }
+    },
+    {
+      name: "1.12.1",
+      dependencies: {
+        "ember": "1.12.1"
+      }
+    }
+  ]
+};
