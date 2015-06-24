c8058eb83353cdd6179482bbc4aa09daec73856e
Robert Jackson <robert.w.jackson@me.com>
2015-06-24 10:52:11 -0400

    Add Ember 1.13 to ember-try configuration.

--- config/ember-try.js
+++ config/ember-try.js
@@ -17,6 +17,12 @@ module.exports = {
       dependencies: {
         "ember": "1.12.1"
       }
+    },
+    {
+      name: "1.13.2",
+      dependencies: {
+        "ember": "1.13.2"
+      }
     }
   ]
 };
