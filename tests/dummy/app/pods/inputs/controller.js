3aa987b27c123b62d655361d9d2126c650f9f80d
Chris Thoburn <runspired@gmail.com>
2015-06-27 00:34:57 -0500

    fix use of observer

--- tests/dummy/app/pods/inputs/controller.js
+++ tests/dummy/app/pods/inputs/controller.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const {
+  Controller,
+  observer,
+  Logger
+  } = Ember;
+
+export default Controller.extend({
   name: 'inputs controller',
   checkbox1Checked: false,
   checkbox2Checked: false,
   checkbox3Checked: false,
-  watchBoxes:function() {
-    console.log('check');
-  }.observes('checkbox1Checked', 'checkbox2Checked', 'checkbox3Checked')
+  watchBoxes: observer('checkbox1Checked', 'checkbox2Checked', 'checkbox3Checked', function() {
+    Logger.debug('check');
+  })
 });
