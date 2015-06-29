674129ef0234443c679a6f7744f1202d89ec0e85
Chris Thoburn <runspired@gmail.com>
2015-06-29 09:21:10 -0500

    preserve pre-glimmer compatibility

--- addon/utils/make-helper-with-args.js
+++ addon/utils/make-helper-with-args.js
@@ -31,7 +31,10 @@ export default function makeHelperWithArgs(component) {
 
     }
 
-    return Ember.Handlebars.helpers.makeViewHelper.call(this, [component], hash, options, options);
+    if (Ember.Handlebars.helpers.makeViewHelper) {
+      return Ember.Handlebars.helpers.makeViewHelper.call(this, [component], hash, options, options);
+    }
+    return Ember.Handlebars.helpers.view.helperFunction.call(this, [component], hash, options, options);
 
   };
 
